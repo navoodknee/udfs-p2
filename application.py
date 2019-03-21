d05afdda99e5998e0fe5774a189c18efae6310ca
vagrant <vagrant@vagrant.vm>
2019-03-21 03:13:56 +0000

    removed project2.py from tracking

---
+++ application.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+"""
+Project 2: Udacity Flask Items/Categories Project
+Author: David Nadwodny
+
+"""
+
+# main landing view
+# lists all categories and list of latest items
+# authenticated users can add items
+
+# Category Detail view
+# lists all items that are in a category
+# authenticated users may edit / delete
+
+# Item Detail view
+# shows details of a specific item
+# authenticated users may edit / delete
+
+# JSON endpoint for entire list
